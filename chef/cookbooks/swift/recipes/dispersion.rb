e933a16a87db967c0c40b84965095d8dafb6553a
Alexander Gordeev <agordeev@mirantis.com>
2012-12-25 15:31:13 +0400

    Workaround for swiftclient for dispersion tools

--- chef/cookbooks/swift/recipes/dispersion.rb
+++ chef/cookbooks/swift/recipes/dispersion.rb
@@ -74,6 +74,12 @@ execute "populate-dispersion" do
   only_if "swift -V 2.0 -U #{service_tenant}:#{service_user} -K '#{service_password}' -A #{keystone_auth_url} stat dispersion_objects 2>&1 | grep 'Container.*not found'"
 end
 
+#TODO(agordeev): remove that workaround once packaged swiftclient be ready
+cookbook_file "/usr/lib/python2.7/dist-packages/swiftclient/client.py" do
+  mode "0664"
+  source "client.py"
+end
+
 template "/etc/swift/dispersion.conf" do
   source     "disperse.conf.erb"
   mode       "0600"
